e7b28c4034138fe0f0995c701ddac083df82cc37
Akli Reguig <akli.reguig@gmail.com>
2015-08-27 23:28:10 +0200

    - second commit

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -28,35 +28,48 @@ func loadPage(title string) (*Page,error) {
 }
 
 
-// Web stuff. 
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage (title)
-	t, _ := template.ParseFiles("view.html")
+// >>>>>>>>> Web stuff. 
+// Renders the html template tmpl filled with p content 
+// in the writer w
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl+".html")
 	t.Execute(w,p)
-	
 }
 
+// Handles /view/* endpoints
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage (title)
+	if err !=nil{
+		http.Redirect(w,r,"/edit/"+title,http.StatusFound)
+	}
+	renderTemplate(w,"view",p)
+}
+
+// Handles /edit/* endpoints
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title:title}
 	}
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(w,p)
+	renderTemplate(w,"edit",p)
 }
 
+// Handle /save/* endpoints
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	//title := r.URL.Path[len("/edit/"):]
-	
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title:title,Body:[]byte(body)}
+	p.save()
+	http.Redirect(w,r,"/view"+title,http.StatusFound)
 }
 
-
+// Main 
 func main() {
 	http.HandleFunc("/view/",viewHandler)
 	http.HandleFunc("/edit/",editHandler)
+	http.HandleFunc("/save/",saveHandler)
 	fmt.Println("Starting the server...")
 	http.ListenAndServe(":8080",nil)
 }
