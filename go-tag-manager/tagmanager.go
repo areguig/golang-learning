3fe3c19f82a056c77f07e3ef492662c5d122dc0c
Akli Reguig <akli.reguig@gmail.com>
2015-09-05 12:39:11 +0200

    - Tag Manager URL parameter parsing exception  - Tag manager html/template test

--- go-tag-manager/tagmanager.go
+++ go-tag-manager/tagmanager.go
@@ -3,23 +3,41 @@ package main
 import (
     "fmt"
     "net/http"
+    "html/template"
     "encoding/base64"
+
+    "appengine"
+    "appengine/user"
 )
 
 func init() {
     http.HandleFunc("/tag",getTag)
+    http.HandleFunc("/",index)
 }
 
 
 
 func getTag(w http.ResponseWriter, r *http.Request) {
-	az := r.URL.Path[len("/tag?az="):]
-	 decoded, _ := base64.URLEncoding.DecodeString(az)
+	 az := r.URL.Query().Get("az")
+   decoded, _ := base64.URLEncoding.DecodeString(az)
 	 fmt.Println("az: ",az,"\n decoded :",string(decoded))
 	 fmt.Fprint(w, string(decoded))
-	
+
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+func index(w http.ResponseWriter, r *http.Request) {
+  c := appengine.NewContext(r)
+  u := user.Current(c)
+  if u == nil {
+    url, err := user.LoginURL(c, r.URL.String())
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    w.Header().Set("Location", url)
+    w.WriteHeader(http.StatusFound)
+    return
+  }
+  t, _ := template.ParseFiles("templates/index.html")
+  t.Execute(w,u)
 }
