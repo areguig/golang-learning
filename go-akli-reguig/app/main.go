94208a77bf18b33b8bcd5ae40d5144b9137e815d
Akli Reguig <akli.reguig@gmail.com>
2016-04-24 23:59:52 +0200

    Front : tweet selector

---
+++ go-akli-reguig/app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+)
+
+func init() {
+	http.HandleFunc("/twitter/search", twitterSearch)
+}
+
+func twitterSearch(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	query := r.URL.Query().Get("q")
+	log.Debugf(ctx, "Twitter search ", string(query))
+	RequestHandler(w, r)
+}
